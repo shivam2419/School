d12e982a78b5aa62c01fddf7d16a78b49f236e3d
shivam2419 <shivam241980@gmail.com>
2025-06-03 16:14:46 +0530

    error fixed

--- School_Project/settings.py
+++ School_Project/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -71,7 +73,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "School_Project.wsgi.application"
 
-
+CORS_ALLOWED_ORIGINS = [
+    "https://school-project-upz6.onrender.com",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://school-project-upz6.onrender.com",
+]
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
